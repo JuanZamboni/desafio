114e28933b3683a69f0384ba84ab36abe4688c21
JP <juanzamboni718@gmail.com>
2022-09-19 23:55:30 -0300

    .

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,23 +9,32 @@ const Cart = () => {
 
 
 
-  return (
+  return ( 
+
     <div className='container my-2'>
+      
+      {
+        cart.leght === 0 
+        ? <>
+            <h2>Tu carrito está vacío</h2>
+             <Link to="/" className="btn btn-primary">Ir a comprar</Link>
+        </>
 
-   <h2>Carrito</h2>
-
-   { cart.map((item) => (
-   <div key={item.id}>
-       <h4>{item.nombre}</h4>
+        : <>  <h2>Carrito</h2>
+             { cart.map((item) => (
+              <div key={item.id}>
+               <h4>{item.nombre}</h4>
                     <p>Precio: {item.precio}</p>
                     <p>Cantidad: {item.cantidad}</p>
                      <button onClick={() => removeItem(item.id)} className='btn btn-danger mx-2'><BsFillTrashFill/></button>
                     <hr/>
-    </div>
-   ))}
-
-   <h4>Total: ${cartTotal()}</h4>
-   <button onClick={emptyCart}  className='btn btn-danger'>Vaciar carrito</button>
+                   </div>
+        ))}
+ 
+             <h4>Total: ${cartTotal()}</h4>
+             <button onClick={emptyCart}  className='btn btn-danger'>Vaciar carrito</button>
+        </>
+       }
     </div>
 
   )
